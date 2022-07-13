d58c16e5e25e3c81f93bdf0a2276a4c1289fe76c
Cyprien BELLEMIN MENARD <belleminmenardcyprien@hotmail.fr>
2022-07-13 18:24:11 +0200

    Adding SCSS mixin // Register layout and stylization

--- src/Page/Register/Register.js
+++ src/Page/Register/Register.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React from 'react';
+import './Register.scss';
 
 export default function Register() {
   return (
-    <p>Register</p>
-  )
+    <div className='register'>
+      <h1 className='register__title'>Inscription</h1>
+      <form action='' className='register-form'>
+        <div className='register-form__input-container'>
+          <label htmlFor='input-email' className='register-form_label'>
+            Email *
+          </label>
+          <input
+            type='email'
+            className='register-form__input'
+            id='input-email'
+          />
+        </div>
+        <div className='register-form__input-container'>
+          <label htmlFor='input-password' className='register-form_label'>
+            Mot de passe *
+          </label>
+          <input
+            type='password'
+            className='register-form__input'
+            id='input-password'
+          />
+        </div>
+        <div className='register-form__input-container'>
+          <label
+            htmlFor='password-confirmation'
+            className='register-form_label'
+          >
+           Confirmez le mot de passe *
+          </label>
+          <input
+            type='password'
+            className='register-form__input'
+            id='password-confirmation'
+          />
+        </div>
+        <div className='register-form__input-container'>
+          <label htmlFor='input-firstname' className='register-form_label'>
+            Prénom *
+          </label>
+          <input
+            type='text'
+            className='register-form__input'
+            id='input-firstname'
+          />
+        </div>
+        <div className='register-form__input-container'>
+          <label htmlFor='input-lastname' className='register-form_label'>
+            Nom *
+          </label>
+          <input
+            type='text'
+            className='register-form__input'
+            id='input-lastname'
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
